f20463781af45030a234e69cc42f5cd40a842dfc
jvmgomes <joao@mgomes.net>
2025-07-14 17:18:15 -0300

    .

--- src/graph.py
+++ src/graph.py
@@ -2,10 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('frequencies.csv')
+df['Abbreviated_ID'] = df['ID'].apply(lambda x: x.split()[0])
 
-df.set_index('ID')[['A','C','G','T']].plot(kind='bar')
+df.set_index('Abbreviated_ID')[['A','C','G','T']].plot(kind='bar')
 plt.ylabel('Frequência (%)')
 plt.title('Frequência relativa dos nucleotídeos de cada sequência')
 plt.legend(title='Nucleotídeo')
+plt.xticks(rotation=45, ha='right', fontsize=8)
 plt.tight_layout()
 plt.show()
